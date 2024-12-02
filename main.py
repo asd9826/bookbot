74131f59ce830881517a58d0d5f9593fc8dee904
asd9826 <anildeo2601@gmail.com>
2024-12-02 13:30:45 +1100

    Refactor code

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 def main():
-    word_count = 0
-    # Read Frankenstein
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read() 
-    # Loop through the sentence 
-    for words in file_contents.split():
-        word_count+=1
-    print(word_count)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 
 main()
