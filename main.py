67551635897eaf5c0f0f545ade75575632e932c9
asd9826 <anildeo2601@gmail.com>
2024-12-02 13:28:32 +1100

    Print the word count for Frankenstein

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 def main():
+    word_count = 0
+    # Read Frankenstein
     with open("books/frankenstein.txt") as f:
         file_contents = f.read() 
-    print(file_contents)   
+    # Loop through the sentence 
+    for words in file_contents.split():
+        word_count+=1
+    print(word_count)
+
 
 main()
